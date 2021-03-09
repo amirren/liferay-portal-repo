d2725eec2bf6ae7f7f29d738c8083f2917db7444
amirren <amirren@caci.co.uk>
2021-03-09 16:24:56 +0000

    portlet-repo

---
+++ mi.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=javax.portlet -DartifactId=portlet-bridge-api -Dversion=2.0.0 -Dfile=portlet-bridge-api-2.0.0.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
