32051d7d80cc09c5bb817ddd606a20fadf895320
amirren <amirren@caci.co.uk>
2021-10-20 10:37:56 +0100

    datastorm

--- mi.bat
+++ mi.bat
@@ -2,4 +2,6 @@ rem mvn install:install-file -DgroupId=javax.portlet -DartifactId=portlet-bridge
 
 rem mvn install:install-file -DgroupId=org.portletfaces -DartifactId=portletfaces-bridge -Dversion=2.0.1 -Dfile=portletfaces-bridge-2.0.1.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 
-mvn install:install-file -DgroupId=org.eclipse -DartifactId=swt -Dversion=3.6.1-win32-win32-x86_64 -Dfile=swt-3.6.1-win32-win32-x86_64.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+rem mvn install:install-file -DgroupId=org.eclipse -DartifactId=swt -Dversion=3.6.1-win32-win32-x86_64 -Dfile=swt-3.6.1-win32-win32-x86_64.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=datastorm -DartifactId=datastorm -Dversion=1.0.00beta2 -Dfile=datastorm-1.0.00beta2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
