eb94c5625a9f927add1d871a73930fa7405f3196
amirren <amirren@caci.co.uk>
2021-10-20 10:33:09 +0100

    eclipse

--- mi.bat
+++ mi.bat
@@ -1,3 +1,5 @@
 rem mvn install:install-file -DgroupId=javax.portlet -DartifactId=portlet-bridge-api -Dversion=2.0.0 -Dfile=portlet-bridge-api-2.0.0.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 
-mvn install:install-file -DgroupId=org.portletfaces -DartifactId=portletfaces-bridge -Dversion=2.0.1 -Dfile=portletfaces-bridge-2.0.1.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+rem mvn install:install-file -DgroupId=org.portletfaces -DartifactId=portletfaces-bridge -Dversion=2.0.1 -Dfile=portletfaces-bridge-2.0.1.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=org.eclipse -DartifactId=swt -Dversion=3.6.1-win32-win32-x86_64 -Dfile=swt-3.6.1-win32-win32-x86_64.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
