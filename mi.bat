02a1585b55d3c8c58b1d683a7d4628771d688404
amirren <amirren@caci.co.uk>
2021-10-20 11:05:35 +0100

    libs

--- mi.bat
+++ mi.bat
@@ -6,4 +6,30 @@ rem mvn install:install-file -DgroupId=org.eclipse -DartifactId=swt -Dversion=3.
 
 rem mvn install:install-file -DgroupId=datastorm -DartifactId=datastorm -Dversion=1.0.00beta2 -Dfile=datastorm-1.0.00beta2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 
-mvn install:install-file -DgroupId=pentaho.kettle -DartifactId=kettle-engine -Dversion=4.0.1-GA -Dfile=kettle-engine-4.0.1-GA.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libbase -Dversion=1.2.2 -Dfile=libbase-1.2.2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libcss -Dversion=1.2.2 -Dfile=libcss-1.2.2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libdocbundle -Dversion=1.2.3 -Dfile=libdocbundle-1.2.3.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libfonts -Dversion=1.2.3 -Dfile=libfonts-1.2.3.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libformat -Dversion=1.2.4 -Dfile=libformat-1.2.4.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libformula -Dversion=1.1.7 -Dfile=libformula-1.1.7.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libformula -Dversion=1.2.3 -Dfile=libformula-1.2.3.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libloader -Dversion=1.2.3 -Dfile=libloader-1.2.3.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libpixie -Dversion=1.2.2 -Dfile=libpixie-1.2.2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=librepository -Dversion=1.2.3 -Dfile=librepository-1.2.3.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libserializer -Dversion=1.2.2 -Dfile=libserializer-1.2.2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libsparkline -Dversion=1.1.2 -Dfile=libsparkline-1.1.2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+rem mvn install:install-file -DgroupId=pentaho-library -DartifactId=libswing -Dversion=1.2.4 -Dfile=libswing-1.2.4.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=pentaho-library -DartifactId=libxml -Dversion=1.2.2 -Dfile=libxml-1.2.2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=. -DcreateChecksum=true
