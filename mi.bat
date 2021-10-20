d7e18b70eb8a7ce9b08a6d666d0a87c1a2b50288
amirren <amirren@caci.co.uk>
2021-10-20 10:46:26 +0100

    pentaho

--- mi.bat
+++ mi.bat
@@ -4,4 +4,6 @@ rem mvn install:install-file -DgroupId=org.portletfaces -DartifactId=portletface
 
 rem mvn install:install-file -DgroupId=org.eclipse -DartifactId=swt -Dversion=3.6.1-win32-win32-x86_64 -Dfile=swt-3.6.1-win32-win32-x86_64.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 
-mvn install:install-file -DgroupId=datastorm -DartifactId=datastorm -Dversion=1.0.00beta2 -Dfile=datastorm-1.0.00beta2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+rem mvn install:install-file -DgroupId=datastorm -DartifactId=datastorm -Dversion=1.0.00beta2 -Dfile=datastorm-1.0.00beta2.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=pentaho.kettle -DartifactId=kettle-engine -Dversion=4.0.1-GA -Dfile=kettle-engine-4.0.1-GA.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
